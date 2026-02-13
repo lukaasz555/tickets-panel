d5bd5bce6fdbeb40075116042bdbda4177d9c6d6
Lukasz <lukasz.zegar555@gmail.com>
2026-02-13 18:11:12 +0100

    chore: init tickets store

---
+++ src/stores/tickets.store.ts
@@ -0,0 +1,39 @@
+import { defineStore } from 'pinia';
+import { getTickets } from '../services/tickets.service';
+import { Ticket } from '../models';
+import type { TicketStatus } from '../enums';
+
+interface TicketStoreState {
+  tickets: Ticket[];
+  selectedStatuses: TicketStatus[];
+}
+
+const baseState = (): TicketStoreState => ({
+  tickets: [],
+  selectedStatuses: []
+});
+
+export const useTicketsStore = defineStore('tickets', {
+  state: baseState,
+  actions: {
+    async fetchTickets() {
+      this.tickets = await getTickets();
+    },
+    async updateTicketStatus(ticketId: number, newStatus: TicketStatus) {
+      const ticket = this.tickets.find((x) => x.id === ticketId);
+      if (ticket) {
+        ticket.setStatus(newStatus);
+      }
+    }
+  },
+  getters: {
+    filteredTickets: (state) => {
+      if (!state.selectedStatuses.length) {
+        return state.tickets;
+      }
+      return state.tickets.filter((ticket) =>
+        state.selectedStatuses.includes(ticket.status)
+      );
+    }
+  }
+});
